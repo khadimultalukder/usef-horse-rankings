fccc198d4d28a5223505be555404640694157483
Khadimul Talukder <khadimultalukder@gmail.com>
2026-06-10 20:29:16 +0600

    Update run_scraper.bat

--- run_scraper.bat
+++ run_scraper.bat
@@ -1,3 +1,4 @@
 @echo off
-cd /d "D:\Mac Drive\Python Project\Python Clients\Jason LaFrance\usef-horse-rankings" 
+set PYTHONUTF8=1
+cd /d "D:\Mac Drive\Python Project\Python Clients\Jason LaFrance\usef-horse-rankings"
 "D:\Mac Drive\Python Project\.venv\Scripts\python.exe" run.py >> log.txt 2>&1
